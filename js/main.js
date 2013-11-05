eb14f95079c1c5df095f866c21d107bf644dc9fa
Lindsay Hooton <lindsay.gareau@gmail.com>
2013-11-04 22:58:23 -0500

    Updates to index and js files.

--- js/main.js
+++ js/main.js
@@ -2,5 +2,33 @@
 
 $('#home').on('pageinit', function(){
          function launchSearch(){
+               window.location="search.js"; 
          };       
+});	
+		
+$('#addItem').on('pageinit', function(){
+
+		var myForm = $('#form1'),
+                errorLink = $("#additemerrorlink");
+                myForm.validate({
+                     invalidHandler: function(form, validator) {
+                     errorLink.click();
+                
+                var html = "";
+                for (var key in validator.submitted) {
+                     var label = $('label[for^="'+ key + '"]').not('[generated]');
+                     console.log(label.text());
+                     var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+                     var fieldName = legend.length ? legend.text() : label.text();
+                     html += "<li>"+ fieldName +"</li>";
+                }
+                $("#additemerror ul").html(html);
+                console.log(html);
+                },
+                submitHandler: function() {
+                     var data = myForm.serializeArray();
+                     storeData(data);
+                     console.log(data);
+                }
+	});
 });
